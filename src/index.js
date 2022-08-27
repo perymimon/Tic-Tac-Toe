7b4f566c13bfb85f0275772decc3e990d46e16ef
perym <pery.mimon@gmail.com>
2022-08-27 21:18:25 +0300

    block other browsers

--- src/index.js
+++ src/index.js
@@ -4,24 +4,40 @@ import React from 'react';
 
 import * as serviceWorker from './serviceWorker';
 import App from './app/App';
-import {BrowserRouter} from "react-router-dom";
-import {createRoot} from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from 'react-dom/client';
 import { Debug } from 'components/organism/debug';
+
+import supportedBrowsers from './supportedBrowsers.js';
+
 
 const rootContainer = document.getElementById('root');
 const root = createRoot(rootContainer);
- // <React.StrictMode>
-root.render(
-    <>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-        <Debug />
-    </>
-)
-// </React.StrictMode>
 
+if (!supportedBrowsers.test(navigator.userAgent)) {
+    alert('Your browser is not supported.');
+    // var href = location.href.replace(/https?/, 'googlechrome')
+    var href = `intent${window.location.href.replace(/https?/, '')};scheme=https;package=com.android.chrome;end`
+    root.render(
+        <div>
+            Your browser is not supported.
+            click <a href={href} target="_blank" rel="noreferrer">here</a> to open it on chrome
+        </div>
+    )
+} else {
 
+    // <React.StrictMode>
+    root.render(
+        <>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+            <Debug />
+        </>
+    )
+    // </React.StrictMode>
+
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/supportedBrowsers.js
@@ -0,0 +1,3 @@
+var reg = 
+/((CPU[ +]OS|iPhone[ +]OS|CPU[ +]iPhone|CPU IPhone OS)[ +]+(15[_.]5|15[_.]([6-9]|\d{2,})|(1[6-9]|[2-9]\d|\d{3,})[_.]\d+)(?:[_.]\d+)?)|((Chromium|Chrome)\/(102\.0|102\.([1-9]|\d{2,})|(10[3-9]|1[1-9]\d|[2-9]\d\d|\d{4,})\.\d+)(?:\.\d+)?)/
+export default reg;
